f1487e47e6a28bc83f198739f5612722e588aa71
Conor Dockry <conordockry@gmail.com>
2020-11-17 20:23:11 -0500

    inc

--- app/App.tsx
+++ app/App.tsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import { Meta, Scripts, Styles, Routes, useGlobalData } from '@remix-run/react'
+import { NavLink as Link } from 'react-router-dom'
 
 export default function App() {
   let data = useGlobalData()
 
   return (
-    <html lang="en">
+    <html lang="en" className="dark-theme">
       <head>
         <meta charSet="utf-8" />
         <Meta />
         <Styles />
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css"
+        /> */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@geist-ui/typography@latest/umd.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@geist-ui/style@latest/dist/style.css"
         />
       </head>
-      <body>
-        <Routes />
+      <body className="max-w-screen-lg p-4 mx-auto">
+        <header>
+          <nav className="flex items-center">
+            <Link className="mr-6" to="/" activeClassName="active-link" end>
+              Home
+            </Link>
+            <Link className="mr-6" to="about" activeClassName="active-link">
+              About
+            </Link>
+          </nav>
+        </header>
+        <main className="p-4">
+          <Routes />
+        </main>
         <Scripts />
         <footer>
           <p>This page was rendered at {data.date.toLocaleString()}</p>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  future: { removeDeprecatedGapUtilities: true, purgeLayersByDefault: true },
+  purge: ['./app/**/*.tsx', './app/**/*.js', './app/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
